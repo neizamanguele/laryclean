3117a48c537a38d14ad8055a4965e7e64c4cd4c4
neizamanguele <neizamanguele@gmail.com>
2026-07-13 16:56:17 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+﻿var links = document.querySelectorAll("nav a");
+
+for (var i = 0; i < links.length; i++) {
+
+    links[i].addEventListener("click", function (e) {
+
+        e.preventDefault();
+
+        var destino = this.getAttribute("href");
+
+        document.querySelector(destino).scrollIntoView(true);
+
+    });
+
+}
+
